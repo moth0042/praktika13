9ba971efe8f61b366419ba31af135c5fcbcb449c
bobylevapa <moth0042@gmail.com>
2025-02-19 08:53:36 +0500

    создание таблицы в бд

--- database.py
+++ database.py
@@ -1,5 +1,17 @@
 import sqlite3 as sq
 
 con = sq.connect('my_database.db')
+cur = con.cursor()
 
+cur.execute('''CREATE TABLE IF NOT EXISTS Users (
+            id INTEGER PRIMARY KEY,
+            username TEXT NOT NULL,
+            email TEXT NOT NULL,
+            age INTEGER)''')
+
+
+
+
+
+con.commit()
 con.close()
