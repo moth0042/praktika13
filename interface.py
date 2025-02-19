4460880fa5d81c34eb2e370279f16eed850a93b1
bobylevapa <moth0042@gmail.com>
2025-02-19 13:11:47 +0500

    скроллбар

--- interface.py
+++ interface.py
@@ -2,7 +2,7 @@ import tkinter as tk
 from tkinter import ttk, messagebox
 import sqlite3
 
-connection = sqlite3.connect('my_database.db')
+connection = sqlite3.connect('Flight.db')
 cur = connection.cursor()
 
 res = cur.execute("SELECT name FROM sqlite_master WHERE type='table';")
@@ -23,7 +23,7 @@ def tables():
     tree['columns'] = columns
     for col in columns:
         tree.heading(col, text=col, anchor=tk.W)
-        tree.column(col, anchor=tk.W, width=100)
+        tree.column(col, anchor=tk.W, minwidth=100, width=100)
     for i in rows:
         tree.insert('', tk.END, values=i)
 
@@ -46,8 +46,12 @@ frame2.pack(fill=tk.BOTH, expand=True, padx=10, pady=10)
 
 scroll = ttk.Scrollbar(frame2, orient=tk.VERTICAL)
 scroll.pack(side=tk.RIGHT, fill=tk.Y)
-tree = ttk.Treeview(frame2, show="headings", yscroll=scroll.set)
+scroll2 = ttk.Scrollbar(frame2, orient=tk.HORIZONTAL)
+scroll2.pack(side=tk.BOTTOM, fill=tk.X)
+tree = ttk.Treeview(frame2, show="headings", yscrollcommand=scroll.set, xscrollcommand=scroll2.set)
 tree.pack(fill=tk.BOTH, expand=True, side=tk.LEFT)
+scroll.config(command=tree.yview)
+scroll2.config(command=tree.xview)
 
 butframe = ttk.Frame(root)
 butframe.pack(fill=tk.X, padx=10, pady=10)
