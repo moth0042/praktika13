f157a25407dd04c96e801c158df5c7736e73dc2e
bobylevapa <moth0042@gmail.com>
2025-02-19 12:17:02 +0500

    создание окна

--- interface.py
+++ interface.py
@@ -1,3 +1,39 @@
 import tkinter as tk
 from tkinter import ttk, messagebox
 import sqlite3
+
+
+root = tk.Tk()
+
+root.geometry("800x600")
+root.resizable(False, False)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+root.mainloop()
